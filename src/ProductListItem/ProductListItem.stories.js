b68149d922257a1b839affc15536056b72009ee8
lauragrassig <laura_grassig@outlook.com>
2021-05-27 18:34:19 -0300

    Add knobs

--- src/ProductListItem/ProductListItem.stories.js
+++ src/ProductListItem/ProductListItem.stories.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ProductListItem from './ProductListItem';
 import { action } from '@storybook/addon-actions';
+import { text, withKnobs } from '@storybook/addon-knobs';
 
-export default {title: 'ProductListItem'};
+export default {
+  title: 'ProductListItem',
+  decorators: [withKnobs],
+};
+
 export const standard = () => (
   <ProductListItem
-    name="Standard Coffee"
-    price="2.50"
+    name={text("name", "Standard Coffee")}
+    price={text("price", "2.50")}
     onAddToCart={action('Add to cart clicked')}
-    imageUrl="https://source.unsplash.com/tNALoIZhqVM/200x100/"
+    imageUrl={text("imageUrl", "https://source.unsplash.com/tNALoIZhqVM/200x100/")}
     />
 );
