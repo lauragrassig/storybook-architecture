3ec1a44b18d8660401efd951df69a5a2b9940074
lauragrassig <laura_grassig@outlook.com>
2021-05-31 18:35:01 -0300

    add comment

--- src/ProductListItem/ProductListItem.js
+++ src/ProductListItem/ProductListItem.js
@@ -3,6 +3,7 @@ import './ProductListItem.css';
 
 export default function ProductListItem({name, price, imageUrl, onAddToCart, isSoldOut, isOnSale}) {
   return (
+    // Create new component for card later
     <div className="card">
       <h2>{name} {isOnSale && "(On Sale)"}</h2>
       <img src={imageUrl} alt=""/>
