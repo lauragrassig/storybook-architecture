aeb83f775674db0a257bbb676cdface69f46dda4
lauragrassig <laura_grassig@outlook.com>
2021-06-17 16:56:46 -0300

    Storybook unit test component

--- src/components/patterns/ProductListItem/ProductListItem.stories.js
+++ src/components/patterns/ProductListItem/ProductListItem.stories.js
@@ -8,31 +8,46 @@ export default {
   decorators: [withKnobs],
 };
 
-export const standard = () => (
+export const Standard = ({
+  name=text("Name", "Standard Coffee"),
+  price=("price", "2.50"),
+  onAddToCart=action("Add to cart clicked"),
+  imageUrl=text("imageUrl", "https://source.unsplash.com/tNALoIZhqVM/200x100/")
+}) => (
   <ProductListItem
-    name={text("name", "Standard Coffee")}
-    price={text("price", "2.50")}
-    onAddToCart={action('Add to cart clicked')}
-    imageUrl={text("imageUrl", "https://source.unsplash.com/tNALoIZhqVM/200x100/")}
+    name={name}
+    price={price}
+    onAddToCart={onAddToCart}
+    imageUrl={imageUrl}
     />
 );
 
-export const soldOut = () => (
+export const SoldOut = ({
+  name=text("Name", "Standard Coffee"),
+  price=("price", "2.50"),
+  onAddToCart=action("Add to cart clicked"),
+  imageUrl=text("imageUrl", "https://source.unsplash.com/tNALoIZhqVM/200x100/")
+}) => (
   <ProductListItem
-    name={text("name", "Standard Coffee")}
-    price={text("price", "2.50")}
-    onAddToCart={action('Add to cart clicked')}
-    imageUrl={text("imageUrl", "https://source.unsplash.com/tNALoIZhqVM/200x100/")}
+    name={name}
+    price={price}
+    onAddToCart={onAddToCart}
+    imageUrl={imageUrl}
     isSoldOut
     />
 );
 
-export const onSale = () => (
+export const OnSale = ({
+  name=text("Name", "Standard Coffee"),
+  price=("price", "2.50"),
+  onAddToCart=action("Add to cart clicked"),
+  imageUrl=text("imageUrl", "https://source.unsplash.com/tNALoIZhqVM/200x100/")
+}) => (
   <ProductListItem
-    name={text("name", "Standard Coffee")}
-    price={text("price", "2.50")}
-    onAddToCart={action('Add to cart clicked')}
-    imageUrl={text("imageUrl", "https://source.unsplash.com/tNALoIZhqVM/200x100/")}
+    name={name}
+    price={price}
+    onAddToCart={onAddToCart}
+    imageUrl={imageUrl}
     isOnSale
     />
 );
--- src/components/patterns/ProductListItem/ProductListItem.test.js
+++ src/components/patterns/ProductListItem/ProductListItem.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import ProductListItem from './ProductListItem';
+import { OnSale, SoldOut, Standard } from './ProductListItem.stories';
 
 it ('shows on sale label when isOnSale', () => {
   const { getByText } = render(<ProductListItem
@@ -22,4 +23,34 @@ it ('disables the button when disabled', () => {
   />)
   expect(getByText("Sold out"))
     .toHaveAttribute('disabled');
+})
+
+it ('calls callback when button Add To Cart pressed', () => {
+  const onAddToCart = jest.fn();
+  const { getByText } = render(<ProductListItem
+    name="Mocha"
+    price={3.50}
+    onAddToCart={onAddToCart}
+  />)
+  fireEvent.click(getByText("Add to Cart"))
+  expect(onAddToCart).toHaveBeenCalled()
+})
+
+it ('storybook - shows on sale label when isOnSale', () => {
+  const { getByText } = render(<OnSale/>)
+  expect(getByText(`Standard Coffee (On Sale)`))
+    .toBeInTheDocument();
+})
+
+it ('storybook - disables the button when disabled', () => {
+  const { getByText } = render(<SoldOut/>)
+  expect(getByText("Sold out"))
+  .toHaveAttribute('disabled');
+})
+
+it ('storybook - calls callback when button Add To Cart pressed', () => {
+  const onAddToCart = jest.fn();
+  const { getByText } = render(<Standard onAddToCart={onAddToCart} />)
+  fireEvent.click(getByText("Add to Cart"))
+  expect(onAddToCart).toHaveBeenCalled()
 })
